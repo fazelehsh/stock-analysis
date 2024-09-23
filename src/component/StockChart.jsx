ae042a8df9a8c61525ce9b51550eb32a2ace9e72
fazeleh shamsi <fazelehshamsi1380@gmail.com>
2024-09-23 13:26:17 +0330

    update chart design

--- src/component/StockChart.jsx
+++ src/component/StockChart.jsx
@@ -2,26 +2,56 @@
 import { Line } from "react-chartjs-2";
 
 const StockChart = (props) => {
-  return <Line data={{
-    datasets: props.stockDataList.map((item,index)=>{
-      return {
-        label: `${item.symbol} ${item.calcData[0].year}`,
-        data: Object.entries(item.calcData[0]).slice(1),
-        fill: false,
-        borderColor: getColorByIndex(index),
-        tension: 0.1
-      }
-    })
-  }} options={{
-    parsing: {
-      xAxisKey: '[0]',
-      yAxisKey: '[1]'
-    }
-  }} />
+  return (
+    <Line
+      data={{
+        datasets: props.stockDataList.map((item, index) => {
+          return {
+            label: `${item.symbol} ${item.calcData[0].year}`,
+            data: Object.entries(item.calcData[0]).slice(1),
+            fill: false,
+            borderColor: getColorByIndex(index),
+            tension: 0.1
+          };
+        })
+      }} options={{
+        parsing: {
+          xAxisKey: '[0]',
+          yAxisKey: '[1]'
+        },
+        scales: {
+          x: {
+
+            grid: {
+              display: false,
+            },
+          },
+          y: {
+            grid: {
+              display: true,
+              drawBorder: false,
+            },
+            ticks: {
+              display: true, // Keeps the labels visible
+            },
+            border: {
+              display: false, // Hides the right border line
+            },
+
+          },
+        },
+        elements: {
+          line: {
+            borderWidth: 2 // Optional: adjust line thickness
+          }
+        }
+      }} />
+
+  );
 }
 
-const getColorByIndex = (index)=>{
-  switch(index){
+const getColorByIndex = (index) => {
+  switch (index) {
     case 0:
       return '#cd6a71';
     case 1:
@@ -30,7 +60,11 @@ const getColorByIndex = (index)=>{
       return '#05e26b';
     case 3:
       return '#5c33fe';
-    case 4:
+    case 4
+
+
+
+      :
       return '#36cd84';
   }
 }
