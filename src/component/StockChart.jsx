2e2d5c410ba2524a674af1cd6ef1c77a7c43c5d1
fazeleh shamsi <fazelehshamsi1380@gmail.com>
2024-09-24 12:00:23 +0330

    small bug fix

--- src/component/StockChart.jsx
+++ src/component/StockChart.jsx
@@ -3,16 +3,29 @@ import { Line } from "react-chartjs-2";
 
 const StockChart = (props) => {
   return (
-    <div>
+    <div className="flex flex-col items-start">
+      <div className="flex w-full">
+      <div className="flex-1">
       <Line
         data={{
           datasets: props.stockDataList.map((item, index) => {
+            const isTSCO = item.symbol === "TSCO-LON"; 
             return {
               label: `${item.symbol} ${item.calcData[0].year}`,
               data: Object.entries(item.calcData[0]).slice(1),
               fill: false,
               borderColor: getColorByIndex(index),
-              tension: 0.1
+              tension: 0.1,
+              //pointRadius: 0,
+              pointRadius: item.symbol === "isTSCO" ? 5 : 0,  
+              pointStyle: (ctx) => {
+                if (isTSCO && ctx.dataIndex === ctx.dataset.data.length - 1) {
+                  return 'rect';  // Square at the last point for TSCO-LON
+                }
+                return 'circle';
+              },
+              pointHoverRadius: 5,
+              showLine: true,
             };
           })
         }} options={{
@@ -34,9 +47,6 @@ const StockChart = (props) => {
               },
               ticks: {
                 display: true,
-              },
-              ticks: {
-                display: true,
                 callback: (value) => `%${value}`
               },
               title: {
@@ -55,20 +65,21 @@ const StockChart = (props) => {
             }
           }
         }} />
-
-      {/* Display Annual Returns as Text */}
-      {props.stockDataList.map((stockData) => {
-        return <div className="mt-6 text-left w-full">
-            <p className="text-lg">
-              <strong>{stockData.symbol} Annual Returns:</strong> {stockData.annualReturns[0].annualReturn}%
-            </p>
         </div>
-      })}
+      </div>
 
+
+<div className="flex justify-start items-center  w-full  items-start space-x-44 flex flex-row mt-4">
+{props.stockDataList.map((stockData) => (
+          <div key={stockData.symbol} className={`text-lg ${stockData.symbol === 'MSFT' ? 'ml-6' : ''}`}>
+            <strong>{stockData.symbol} Annual Return:</strong> {stockData.annualReturns[0].annualReturn}%
+          </div>
+        ))}
+      </div>
     </div>
-
   );
 }
+
 
 const getColorByIndex = (index) => {
   switch (index) {
